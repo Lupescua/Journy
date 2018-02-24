c0e616a40dd25c08df48ea07d0d2ebe69b22b649
Adrian <lupescua@gmail.com>
2018-02-24 16:55:40 +0100

    Working on the Nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,44 +23,29 @@
 <body>
 
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-around" stily ="width:1000vw">
         <span class="navbar-toggler-icon " href="#">
             <img src="http://www.ryanhamilton.ca/Logos/logo_18.jpg" width="30" height="30" alt="Logo">
         </span>
+
         <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto navbar-brand">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-
-            </ul>
-
-            <form class="form-inline ">
+            <form class="form-inline mr-auto ">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
 
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="nav-item dropdown my-2 my-lg-0">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
                     aria-expanded="false">
-                    <span class="navbar-toggler-icon " href="#">
+                    <span class="navbar-toggler-icon" href="#">
                         <img src="https://www.atomix.com.au/media/2015/06/atomix_user31.png" width="30" height="30" alt="User_face">
+                        <a width="30" height="30"></a>
                     </span>
-
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
