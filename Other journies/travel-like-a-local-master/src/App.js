6b7ca4c526cdd1898e4f1ab55de1059f17c7da72
Adrian <lupescua@gmail.com>
2018-02-25 10:51:01 +0100

    Nav & jumbotron
    Replaced span to Jumbotron
    Changed the type of Nav

--- Other journies/travel-like-a-local-master/src/App.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./jsx/App.jsx').default;
-
-// username coming from express user session
-ReactDOM.render(<App username={username}/>, document.getElementById('app'));
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,17 +1,18 @@
-@extends('layout')
-@section('title')
-Journy
-@endsection
-
-
-@section('content')
-    <img src="https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg" width="100%" height="80%" style="position : absolute" alt="Boackground" >
+@extends('layout') @section('title') Journy @endsection @section('content')
+<style>
+    .jumbotron {
+        background: url('https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg');
+        width:100%;
+        height:80%;
+        position : absolute;
+    }
+</style>
+<div class="jumbotron jumbotron-fluid">
     <form class="form-inline" style="position:relative; " method="post">
-        <input class="form-control  font-weight-bold" style="background: #fff;margin:auto;"type="search" placeholder="Search" aria-label="Search" style="background-color:rgba(0,0,0,0);">
+        <input class="form-control  font-weight-bold mr-auto " style="background: #fff; margin-left:auto; margin-right:0;" type="search"
+            placeholder="Search" aria-label="Search">
     </form>
+</div>
 
-    <form class="form-inline mr-auto ">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+
 @endsection
