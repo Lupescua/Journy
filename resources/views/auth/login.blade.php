29d01b2faf3fd964d55b843a0efd679e4dbe0522
Jiřina Trojánková <jirina.trojankova@seznam.cz>
2018-03-11 16:07:19 +0100

    Added img of Instagram login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,8 @@ Journy-login
                         <a href="{{url('/redirect')}}"><img src="/img/fb.png" alt="facebook login" height="70rem"></a>
                         <br><br>
                         <img src="/img/google.png" alt="google login" height="71rem">
+                        <br><br>
+                        <img src="/img/instagram.png" alt="instagram login" height="71rem" style="border-radius: 5px">
                     </div>
                 </form>
             </div>
--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -14,7 +14,10 @@
                 <a href="{{url('/redirect')}}"><img src="/img/fb.png" alt="facebook login" height="70rem"></a>
                 <br><br>
                 <img src="/img/google.png" alt="google login" height="71rem">
-            
+                <br><br>
+                <img src="/img/instagram.png" alt="instagram login" height="71rem" style="border-radius: 5px">
+
+
         </form>
       </div>
       <div class="modal-footer">
