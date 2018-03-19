7fe8e8486717e9479f661f065b395129a08b86e6
Bartek <bakos@o2.pl>
2018-03-19 10:16:48 +0100

    made cards on location

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -1,48 +1,16 @@
-@extends('layout')
-@section('title')
-Journy
-@endsection
+@extends('layout') @section('title') Journy @endsection @section('content')
 
- <style>
-        .container {
-            width: 80vw;
-            margin: auto;
-            padding: 0;
-        }
-
-        .location_main_picture{
-            background: url('C:/BartProjects/codingbootcamp/AJB/MyFilesAndResources/HoiAn.jpg');
-            height: 500vh;
-            margin: 0;
-            text-align: center;
-
-        /*position: absolute;
-        background-size: cover;
-        display: flex; */
-        }
-    </style>
-
-
-@section('content')
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Journy Location</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-<div class="jumbotron location_main_picture p-3 p-md-5 text-white rounded bg-dark"</div>
+<div class="jumbotron location_main_picture p-3 p-md-5 text-white     text-shadow: 2px 2px 4px #000000; rounded bg-dark">
   <h1>Locations Gallery and Choice</h1>
   <p>The Journey have many options of spendig free time with Friendly Locals!</p>
-  <p>You can just click interested you photo and see what attractions are waiting for you and who can be your guide in chosen time.</p>
+  <p>You can just click interested you photo and see what attractions are waiting for you and who can be your guide in chosen
+    time.
+  </p>
   <p>Click on the images to see the details</p>
+</div>
+
+<div class="container row">
+
   <div class="row">
     <div class="col-md-4">
       <div class="thumbnail">
@@ -140,15 +108,5 @@ Journy
     </div>
   </div>
 </div>
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</body>
-</html>
-
-
-
 
 @endsection
