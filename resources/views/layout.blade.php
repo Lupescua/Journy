f319c980a129ec072a3fa0d7e47b72316462752f
Bartek <bakos@o2.pl>
2018-03-21 14:58:02 +0100

    periodic save

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -122,6 +122,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon">
+                
             </span>
         </button>
 
