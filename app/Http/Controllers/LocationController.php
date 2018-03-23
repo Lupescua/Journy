a2b89c2173e1b309de951109ea66477fefef7caa
Bartek <bakos@o2.pl>
2018-03-23 09:50:14 +0100

    adding search and inputs in location

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
-    public function index(){
-        return view('location');
+    public function index(Request $request){
+        $search = $request->input('search');
+        $filter1 = $request->input('filter1');
+        $filter2 = $request->input('filter2');
+        $filter3 = $request->input('filter3');
+
+        return view('location', compact(['search', 'filter1', 'filter2', 'filter3']));
     }
     public function index2(){
         return view('experience.experience-old-to-delete');
--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -1,112 +1,336 @@
 @extends('layout') @section('title') Journy @endsection @section('content')
+<div class="jumbotron location_main_picture p-md-4 text-white rounded bg-dark">
+  <div class="row">
+    <h1 class="col-md-5">Locations Gallery and Choice</h1>
+    <p class="col-md-2">The Journey have many options of spending free time with Friendly Locals !</p>
+    <p class="col-md-2">You can just click interested you photo and see what attractions are waiting for you and who can be your guide in chosen
+      time. </p>
+    <p class="col-md-2 p">Click on the images to see the details.</p>
+    <form class="form-inline" action="{{ action('LocationController@index') }}"
+      method="get">
+      <div class="form-group">
+        <label for="exampleInputName2"></label>
+        <input type="text" name="search" class="form-control" id="exampleInputName1" placeholder="Place from homepage"
+          value="{{ $search }}">
+      </div>
 
-<div class="jumbotron location_main_picture p-3 p-md-5 text-white     text-shadow: 2px 2px 4px #000000; rounded bg-dark">
-  <h1>Locations Gallery and Choice</h1>
-  <p>The Journey have many options of spendig free time with Friendly Locals!</p>
-  <p>You can just click interested you photo and see what attractions are waiting for you and who can be your guide in chosen
-    time.
-  </p>
-  <p>Click on the images to see the details</p>
+      @if(isset($search))
+      <span class="badge badge-secondary">
+        <i class="fa fa-plus"></i>
+      </span>
+      <div class="form-group">
+        <label for="exampleInputName2"></label>
+        <input type="text" name="filter1" class="form-control" id="exampleInputName2" placeholder="Activity" value="{{ $filter1 }}">
+      </div>
+      @endif
+
+      @if(isset($filter1))
+      <span class="badge badge-secondary">
+        <i class="fa fa-plus"></i>
+      </span>
+      <div class="form-group">
+        <label for="exampleInputName2"></label>
+        <input type="text" name="filter2" class="form-control" id="exampleInputName3" placeholder="Additional activity"
+          value="{{ $filter2 }}">
+      </div>
+      @endif
+
+      @if(isset($filter2))
+      <span class="badge badge-secondary">
+        <i class="fa fa-plus"></i>
+      </span>
+      <div class="form-group">
+        <label for="exampleInputName2"></label>
+        <input type="text" name="filter3" class="form-control" id="exampleInputName4" placeholder="More activities"
+          value="{{ $filter3 }}">
+      </div
+      >@endif
+      <div class="form-group">
+        <button type="submit" class="btn btn-outline-light">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+      <!-- <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <br>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a>
+      <a class="btn btn-secondary" href="#" role="button">Link</a> -->
+
+
+    </form>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-2 img_prop rounded bg-dark">
+      <div class="thumbnail">
+        <a href="" target="_blank">
+          <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+          <div class="caption">
+            <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+          </div>
+        </a>
+      </div>
+    </div>
 </div>
 
-<div class="container row">
+<div class="row">
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\normand.jpg" alt="Normandie" style="width:100%">
+        <div class="caption">
+          <p class="dx">Normandie Sightseeing with Antoine</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\bali_local.jpg" alt="Bali" style="width:100%">
+        <div class="caption">
+          <p class="dx">Trip on Bali with Yaya</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\philipina.jpg" alt="Phillipina" style="width:100%">
+        <div class="caption">
+          <p class="dx">Phillipina Heights with Coco</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
 
-  <div class="row">
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="C:\\BartProjects\codingbootcamp\AJB\MyFilesAndResources\HoiAn.jpg" target="_blank">
-          <img src="C:\\BartProjects\codingbootcamp\AJB\MyFilesAndResources\hoian.jpg" alt="hoian.jpg" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="/w3images/nature.jpg" target="_blank">
-          <img src="/w3images/nature.jpg" alt="Nature" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="/w3images/fjords.jpg" target="_blank">
-          <img src="/w3images/fjords.jpg" alt="Fjords" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
+<div class="row">
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\PolandMarket.jpg" alt="Agnieszka" style="width:100%">
+        <div class="caption">
+          <p class="dx">Paths of finding Polish healthy food with Agnieszka</p>
+        </div>
+      </a>
     </div>
   </div>
-  <div class="row">
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="lights.jpg" target="_blank">
-          <img src="lights.jpg" alt="Lights" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="nature.jpg" target="_blank">
-          <img src="nature.jpg" alt="Nature" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="fjords.jpg" target="_blank">
-          <img src="fjords.jpg" alt="Fjords" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\camodia-bike.jpg" alt="Cambodia" style="width:100%">
+        <div class="caption">
+          <p>Cambodia from the scooter with Brad</p>
+        </div>
+      </a>
     </div>
   </div>
-  <div class="row">
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="lights.jpg" target="_blank">
-          <img src="lights.jpg" alt="Lights" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="nature.jpg" target="_blank">
-          <img src="nature.jpg" alt="Nature" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-        <a href="fjords.jpg" target="_blank">
-          <img src="fjords.jpg" alt="Fjords" style="width:100%">
-          <div class="caption">
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-          </div>
-        </a>
-      </div>
+  <div class="col-md-2 img_prop raunded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\mongolia_for_hipsters.jpg" alt="Mongolia" style="width:100%">
+        <div class="caption">
+          <p class="dx">Mongolia for hipsters with Kris</p>
+        </div>
+      </a>
     </div>
   </div>
+</div>
+
+<div class="row">
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\harajuku.jpg" alt="harajuku" style="width:100%">
+        <div class="caption">
+          <p class="dx">Shopping with Harajuku Girls in Tokio.</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\locals_siberia.jpg" alt="siberia" style="width:100%">
+        <div class="caption">
+          <p class="dx">Siberian trekking with Nikita and Sasha</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\newzealand.jpg" alt="New Zealand" style="width:100%">
+        <div class="caption">
+          <p class="dx">New Zealand experience with Aria and Samuel</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\normand.jpg" alt="Normandie" style="width:100%">
+        <div class="caption">
+          <p class="dx">Normandie Sightseeing with Antoine</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\bali_local.jpg" alt="Bali" style="width:100%">
+        <div class="caption">
+          <p class="dx">Trip on Bali with Yaya</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\philipina.jpg" alt="Phillipina" style="width:100%">
+        <div class="caption">
+          <p class="dx">Phillipina Heights with Coco</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\PolandMarket.jpg" alt="Agnieszka" style="width:100%">
+        <div class="caption">
+          <p class="dx">Paths of finding Polish healthy food with Agnieszka</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop rounded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\camodia-bike.jpg" alt="Cambodia" style="width:100%">
+        <div class="caption">
+          <p>Cambodia from the scooter with Brad</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-2 img_prop raunded bg-dark">
+    <div class="thumbnail">
+      <a href="" target="_blank">
+        <img src="\img\mongolia_for_hipsters.jpg" alt="Mongolia" style="width:100%">
+        <div class="caption">
+          <p class="dx">Mongolia for hipsters with Kris</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
 </div>
 
 @endsection
