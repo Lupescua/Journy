3bc429b21357470a2c6c9e82e1d1643fa23ab59a
lupuescua <lupescua@gmail.com>
2018-03-27 17:21:39 +0200

    fixed conflict

--- app/Experience.php
+++ app/Experience.php
@@ -6,13 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Experience extends Model
 {
-<<<<<<< HEAD
     protected $table = 'experiences';
     protected $fillable = [
         'name', 'user_id','main_img','rating','main_tags','price','currency','max_participants','video_id','description','location_id','address','duration','offered','language','sport_level','about_host','what_we_do','what_we_provide','who_can_come','where_we_be','location_lng','location_lat',
     ];
-=======
-    protected $table = "experiences";
->>>>>>> BartiBranch
 }
 
