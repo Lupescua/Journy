3de13f2b4d3c2f6ff1087970a9fe77e8d1f81c23
lupuescua <lupescua@gmail.com>
2018-03-27 17:40:15 +0200

    fixed seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -33,7 +33,7 @@ class DatabaseSeeder extends Seeder
         \App\Experience::create([
             'name'=>'Long is the night we\'ve been waiting for',
             'user_id'=>'1',
-            'main_img'=>'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg',
+            'image'=>'https://static.pexels.com/photos/414171/pexels-photo-414171.jpeg',
             'rating'=>'3',
             'main_tags'=>'dragons,ice,fire',
             'people_who_did_it'=>'55',
@@ -43,7 +43,7 @@ class DatabaseSeeder extends Seeder
             'video_id'=>'http://videos.criticalcommons.org/transcoded/http/ccserver.usc.edu/8080/cc/Members/MCIMR/clips/playmobil-game-of-thrones-2012.mp4/mp4-low/playmobil-game-of-thrones-2012-mp4.mp4',
             'description'=>'A personal, face-to-face encounter with this awesome animal, is guaranteed to get the adrenaline going! Dive with the sharks in Gaansbaai! Getting into the cage with the sharks around, is truly one of the most breath-taking adventure activities you will ever live to remember. When feeding actively around the boat, the sharks may occasionally brush their tail against the cage, but they NEVER attack the cage. They are very curious though and often come close-up to the cage, to take a closer look at the diver.',
             'location_id'=>'Prague',
-            'address'=>'',
+            'place'=>'',
             'duration'=>'3.5',
             'offered'=>'1 meal, Drinks and EquipmentJon Snow',
             'language'=>'English',
@@ -53,8 +53,8 @@ class DatabaseSeeder extends Seeder
             'what_we_provide'=>'Home made cake 󴀁 Home made lemonade 󲀃 Materials for your book 󳀁 Choose from a great variety of old printed materials relating to Czech history and cultureHome made cake 󴀁 Home made lemonade 󲀃 Materials for your book 󳀁 Choose from a great variety of old printed materials relating to Czech history and culture',
             'who_can_come'=>'Guests ages 12 and up can attend.',
             'where_we_be'=>'Mordor is not that far, but this is the land of Fire and Ice. We will be camping in the forests of Mithelhime and eat with the Trolls unde the bridge of Happyness. Luckylly, our music will be offered by noneother thatn Tyrion Drake Malfoy and his acompaniament of Jolly Rogers.',
-            'location_lng'=>'54343',
-            'location_lat'=>'123456',
+            'gps_lng'=>'54343',
+            'gps_lat'=>'123456',
         ]);
         
         $continent = Continent::create([
