31172d5fcb013cddc1516d4846a483b1518610af
Adrian <lupescua@gmail.com>
2018-03-29 16:03:22 +0200

    Changed redirect form FB login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/new_user';
 
     /**
      * Create a new controller instance.
