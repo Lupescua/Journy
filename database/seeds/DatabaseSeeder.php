63bfb179678a29b9163779d974fdb31807af911e
lupuescua <lupescua@gmail.com>
2018-03-30 14:59:52 +0200

    some seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
             'user_tags'=>'snow,winter,wolves',
         ]);
 
-        
+
         $continent = Continent::create([
             'name' => 'asia'
         ]);
@@ -432,6 +432,35 @@ class DatabaseSeeder extends Seeder
                     'region_id' => $region->id,
                     'name' => 'sightseeing'
                 ]);
+                        Experience::create([
+                            'continent_id' => $continent->id,
+                            'region_id' => $region->id,
+                            'activity_id' => $activity->id,
+                            'name' => 'Book a Journy',
+                            'image' => '28279965_1593086544078954_2118624544300176229_n.jpg',
+                            'user_id'=>'1',
+                            'rating'=>'5',
+                            'main_tags'=>'',
+                            'people_who_did_it'=>'3',
+                            'price'=>'200',
+                            'currency'=>'EUR',
+                            'max_participants'=>'50',
+                            'video_id'=>'',
+                            'description'=>'Hello everybody and wellcome to our project\'s web platform. We have created this "JournY" in order to help travellers that are searching real experience in places where they might be completely lonely and to help local people, who have a great knowledge of a place where they live or they might have some skills that thay can teach others.',
+                            'location_id'=>'',
+                            'place'=>'Prague',
+                            'duration'=>'15 min',
+                            'offered'=>'Fun',
+                            'language'=>'English',
+                            'sport_level'=>'1',
+                            'about_host'=>'We are a group of three young people, Adrian from Romania, who came with this idea several years ago and finally got the chance to bring his long awaited project to life... and Bartek from Poland and Jirina from Czech republic who were happy to form the team and create this page.',
+                            'what_we_do'=>'We\'ve worked really hard but also have had lots of fun while creating this pages. Hope you will enjoy your browsing here and we will be the most happy people if you get the chance to meet your local guides on your travel through "JournY". Or if you are a person who have things to offer don"t hesitate and use our "JournY" to present your skills.',
+                            'what_we_provide'=>'',
+                            'who_can_come'=>'Anybody who is looking for an euthentic travel experiene',
+                            'where_we_be'=>'Startup Center',
+                            'gps_lng'=>'',
+                            'gps_lat'=>'',
+                        ]);
 
         $continent = Continent::create([
             'name' => 'america'
